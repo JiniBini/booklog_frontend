9283c0a8f9aef9ce9bc4b16aa373ed4a60491cd5
Kim-Eun-Bin <kiobn1201@gmail.com>
2022-04-28 23:24:48 +0900

    first commit

---
+++ src/components/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+
+const MainPage = () => {
+
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default MainPage;
