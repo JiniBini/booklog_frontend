cb4744d65328aab45a6b98a091f21e81434561cf
Kim-Eun-Bin <kiobn1201@gmail.com>
2022-04-30 23:47:16 +0900

    modify header
    - header.js
    - styles.scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Header = () => {
-
   return (
-    <div>
-      <h1>header</h1>
+    <div className="header-container">
+      <div>
+        <a className="login-btn" href="/login">LOGIN</a>
+      </div>
+      <div className="header-content">
+        <li>
+          <ul><a className="list-item" href="/board">board</a></ul>
+          <ul><a className="list-item" href="/photo">photo</a></ul>
+        </li>
+        <a className="header-title" href="/">𝔹𝕠𝕠𝕜𝕃𝕠𝕘</a>
+        <li>
+          <ul><a className="list-item" href="/guestbook">guestbook</a></ul>
+          <ul><a className="list-item" href="/setting">setting</a></ul>
+        </li>
+      </div>
     </div>
   )
 }
